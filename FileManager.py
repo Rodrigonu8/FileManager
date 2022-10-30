c9e8b701cc515917426830b4693cc0d0e3d943d7
Rodrigonu8 <rod.nunezv@alumnos.duoc.cl>
2022-10-29 22:31:12 -0300

    Organizador de archivos Office, pdf y comprimidos

--- FileManager.py
+++ FileManager.py
@@ -1,5 +1,6 @@
 # Administrador de archivos automatizado.
 from distutils import extension
+from gettext import install
 from pkgutil import extend_path
 from PIL import Image
 import os
@@ -7,15 +8,44 @@ import os
 downloadsFolder = "C:/Users/rodri/Downloads/"
 picturesFolder = "C:/Users/rodri/OneDrive/Pictures/"
 
+
 if __name__ == "__main__":
     for filename in os.listdir(downloadsFolder):
         name, extension = os.path.splitext(downloadsFolder + filename)
 
         if extension in [".jpg", ".jpeg", ".png"]:
             picture = Image.open(downloadsFolder + filename)
-            picture.save(picturesFolder + "compressed_"+filename, optimize=True, quality=60)
+            picture.save(picturesFolder + "compressed_" +
+                         filename, optimize=True, quality=60)
             os.remove(downloadsFolder + filename)
             print(name + ": " + extension)
 
+        if extension in [".exe"]:
+            installFolder = "C:/Users/rodri/Instaladores/"
+            os.rename(downloadsFolder + filename, installFolder + filename)
+            print(name + ": " + extension)
 
+        if extension in [".docx"]:
+            wordFolder = "C:/Users/rodri/OneDrive/Word/"
+            os.rename(downloadsFolder + filename, wordFolder + filename)
+            print(name + ": " + extension)
 
+        if extension in [".xlsx"]:
+            excelFolder = "C:/Users/rodri/OneDrive/Excel/"
+            os.rename(downloadsFolder + filename, excelFolder + filename)
+            print(name + ": " + extension)
+
+        if extension in [".pptx"]:
+            pptsFolder = "C:/Users/rodri/OneDrive/PowerPoint/"
+            os.rename(downloadsFolder + filename, pptsFolder + filename)
+            print(name + ": " + extension)
+        
+        if extension in [".pdf"]:
+            pdfFolder = "C:/Users/rodri/OneDrive/Pdf/"
+            os.rename(downloadsFolder + filename, pdfFolder + filename)
+            print(name + ": " + extension)
+
+        if extension in [".zip", ".rar"]:
+            compFolder = "C:/Users/rodri/OneDrive/Comprimido/"
+            os.rename(downloadsFolder + filename, compFolder + filename)
+            print(name + ": " + extension)

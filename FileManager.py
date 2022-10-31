b6211c0cdca8331afa981673d05c80c857bf879d
Rodrigonu8 <rod.nunezv@alumnos.duoc.cl>
2022-10-31 16:33:47 -0300

    Script Listo

--- FileManager.py
+++ FileManager.py
@@ -5,8 +5,8 @@ from pkgutil import extend_path
 from PIL import Image
 import os
 
-downloadsFolder = "C:/Users/rodri/Downloads"
-picturesFolder = "C:/Users/rodri/OneDrive/Pictures"
+downloadsFolder = "C:/Users/rodri/Downloads/"
+picturesFolder = "C:/Users/rodri/OneDrive/Pictures/"
 
 
 if __name__ == "__main__":
@@ -26,26 +26,26 @@ if __name__ == "__main__":
             print(name + ": " + extension)
 
         if extension in [".docx"]:
-            wordFolder = "C:/Users/rodri/OneDrive/Word/"
+            wordFolder = "C:/Users/rodri/OneDrive - Emtec Group/Documentos/Word/"
             os.rename(downloadsFolder + filename, wordFolder + filename)
             print(name + ": " + extension)
 
         if extension in [".xlsx"]:
-            excelFolder = "C:/Users/rodri/OneDrive/Excel/"
+            excelFolder = "C:/Users/rodri/OneDrive - Emtec Group/Documentos/Excel/"
             os.rename(downloadsFolder + filename, excelFolder + filename)
             print(name + ": " + extension)
 
         if extension in [".pptx"]:
-            pptsFolder = "C:/Users/rodri/OneDrive/PowerPoint/"
+            pptsFolder = "C:/Users/rodri/OneDrive - Emtec Group/Documentos/Ppt/"
             os.rename(downloadsFolder + filename, pptsFolder + filename)
             print(name + ": " + extension)
         
         if extension in [".pdf"]:
-            pdfFolder = "C:/Users/rodri/OneDrive/Pdf/"
+            pdfFolder = "C:/Users/rodri/OneDrive - Emtec Group/Documentos/Pdf/"
             os.rename(downloadsFolder + filename, pdfFolder + filename)
             print(name + ": " + extension)
 
         if extension in [".zip", ".rar"]:
-            compFolder = "C:/Users/rodri/OneDrive/Comprimido/"
+            compFolder = "C:/Users/rodri/Comprimido/"
             os.rename(downloadsFolder + filename, compFolder + filename)
             print(name + ": " + extension)
